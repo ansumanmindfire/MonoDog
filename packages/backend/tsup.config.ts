bd565070101765984148078fecaa90861213d8a0
ansumanmindfire <ansumanp@mindfiresolutions.com>
2026-07-28 11:45:32 +0530

    fix(release): modify tsup to build only cli.js

--- packages/backend/tsup.config.ts
+++ packages/backend/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/cli.ts', 'src/index.ts'],
+  entry: ['src/cli.ts'],
   format: ['cjs'],
   target: 'node18',
   clean: true,
